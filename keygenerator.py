7d58e7e97f42e129bde42b3b64040c3797937fd2
Emir Ali Kıvrak <emirccus@gmail.com>
2020-05-02 14:48:27 +0300

    key classlar olusturuldu hala 4. adımda hata var

--- keygenerator.py
+++ keygenerator.py
@@ -1,7 +1,39 @@
 from random import getrandbits,randrange,randint
 from math import gcd ## gcd == ebob
+
+class PrivateKey:
+
+    def __init__(self,lambda_val,micro):
+        print("Gizli anahtar oluşturuluyor")
+        self.l = lambda_val
+        self.m = micro
+        print("Gizli anahtar oluşturuldu")
+
+    def printKey(self):
+        print("** PRIVATE KEY ** ")
+        print("lambda değeri : ",self.l)
+        print("mikro değeri  :",self.m)
+        print("** PRIVATE KEY **")
+
+class PublicKey:
+
+    def __init__(self,n,g):
+        print("Açık anahtar oluşturuluyor")
+        self.n = n
+        self.g = g
+        print("Açık anahtar Oluşturuldu.")
+        
+        
+    def printKey(self):
+        print("** PUBLIC KEY ** ")
+        print("n değeri : ",self.n)
+        print("g değeri  :",self.g)
+        print("** PUBLIC KEY **")
+
+
 class PaillierKeyGenerator:
     def generate_paillier_key_pairs(self):
+        print("Anahtar çifti üretme işlemi başlıyor..")
         q = self.generate_random_primary_number()
         p = self.generate_random_primary_number()
         print("p ve q değerleri oluşturuldu.")
@@ -21,6 +53,7 @@ class PaillierKeyGenerator:
         print("mikro değeri oluşturuldu")
 
     def generate_random_primary_number(self,numbers_of_test = 3,bits = 1024):
+        print("Rastgele '",bits,"' bit asal sayı üretme islemi yapılacak '",numbers_of_test,"' kere test edilecek")
         """Rastgele büyük bir sayı üretip primality teste sokacağız eğer başarılı ise
         generation_successfull değeri true değişken alacak ve fonksiyon geriye sayıyı döndürecek 
         bu fonksiyon iki büyük p ve q asal sayısını üretmek için yazılıyor"""
